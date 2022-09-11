369895e9ff13d5c200b5d41f78752507923fb766
Johannes Bräuer <yanko397@web.de>
2022-09-11 15:37:05 +0200

    initial commit (code from project furry-octo-succotash)

---
+++ functions.js
@@ -0,0 +1,23 @@
+
+function getRandomColor() {
+	var letters = '0123456789ABCDEF';
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+
+function setRandomColor() {
+	var randomColor = getRandomColor();
+	document.body.style.background = randomColor;
+}
+
+function headache() {
+	while (1) {
+		setRandomColor();
+	}
+}
+
+document.onclick = function(){getRandomColor()}
+document.onload = function(){headache()}
